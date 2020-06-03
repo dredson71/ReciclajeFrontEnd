f44d722a857354bf24266b15a1b3dca8215828c8
JALG99 <jorgelandaverry@hotmail.com>
2020-06-03 11:10:48 -0500

    Asociación

---
+++ reciclaje/src/components/Asociacion.vue
@@ -0,0 +1,191 @@
+<template>
+  <v-layout align-start>
+    <v-flex>
+      <v-toolbar flat color="white">
+        <v-toolbar-title>Asociación</v-toolbar-title>
+        <v-divider class="mx-2" inset vertical></v-divider>
+        <v-spacer></v-spacer>
+        <v-text-field
+          class="text-xs-center"
+          v-model="search"
+          append-icon="search"
+          label="Búsqueda"
+          single-line
+          hide-details
+        ></v-text-field>
+        <v-spacer></v-spacer>
+         <v-btn 
+         @click="open" color="primary">
+            Nuevo
+      </v-btn>
+        <v-dialog v-model="dialog" max-width="500px">
+          <v-card>
+            <v-card-title>
+              <span class="headline">{{ formTitle }}</span>
+            </v-card-title>
+
+            <v-card-text>
+              <v-container grid-list-md>
+                <v-layout wrap>
+                  <v-flex xs12 sm12 md12>
+                    <v-text-field v-model="nombre" label="Nombres"></v-text-field>
+                  </v-flex>
+                </v-layout>
+              </v-container>
+            </v-card-text>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+               <v-btn 
+          @click.native="close" color="primary">
+            Cancelar
+      </v-btn>
+      <v-btn 
+          @click.native="guardar" color="primary">
+            Guardar
+      </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-toolbar>
+      <v-data-table
+    :headers="headers"
+    :items="asociacion"
+    class="elevation-1"
+    :search="search"
+  >
+    <template v-slot:item.actions="{ item }">
+     
+
+      <router-link :to="'/asociacion/' + item.codigo ">
+       <v-btn icon >
+              <v-icon>info</v-icon>
+            </v-btn>
+      </router-link>
+
+  
+    </template>
+
+
+
+    <template v-slot:no-data>
+      <v-btn color="primary" >Reset</v-btn>
+    </template>
+  </v-data-table>
+
+
+    </v-flex>
+  </v-layout>
+</template>
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      asociacion: [],
+      dialog: false,
+      headers: [
+        {text: 'ID',value: 'codigo',sortable:false},
+        { text: "Nombres", value: "nombre", sortable: false },
+        {text: 'Actions',value: 'actions',sortable:false}
+      ],
+      search: "",
+      editedIndex: -1,
+
+      //TODO:Model
+      codigo: '',
+      nombre:''
+    };
+  },
+  computed: {
+    formTitle() {
+      return this.editedIndex === -1 ? "Nueva Asociacion" : "Actualizar Asociacion";
+    }
+  },
+
+  watch: {
+    dialog(val) {
+      val || this.close();
+    }
+  },
+
+  created() {
+    //TODO
+    this.listar();
+
+  },
+  methods: {
+    listar() {
+      //TODO
+      let me=this;
+      axios
+      .get("/asociacion")
+      .then(function(response){me.asociacion=response.data;})
+      .catch(function(error){console.log(error);});
+    },
+    editItem(item) {
+      //TODO
+      this.codigo=item.codigo;
+      this.nombre=item.nombre;
+
+      this.editedIndex=1;
+      this.dialog=true;
+    },
+    close() {
+      this.dialog = false;
+      this.limpiar();
+      this.editedIndex = -1;
+    },
+
+    open(){
+      this.dialog = true;
+    },
+    limpiar() {
+      this.codigo = "";
+      this.nombre = "";
+    },
+    deleteItem(item,id){
+      let me = this;
+      axios.delete("/asociacion/"+item.codigo)
+      .then(function(response){
+             me.listar();
+         })
+    },
+    guardar() {
+     //TODO
+     if(this.editedIndex> -1){
+         let me =this;
+         axios
+         .put("/asociacion",{
+             codigo:me.codigo,
+             nombre:me.nombre
+         })
+         .then(function(response){
+             me.close();
+             me.listar();
+             me.limpiar();
+         })
+         .catch(function(error){
+             console.log(error);
+         });
+     }
+     else {
+         let me=this;
+         axios
+         .post("/asociacion",{
+             nombre:me.nombre
+         })
+         .then(function(response){
+             me.close();
+             me.listar();
+             me.limpiar();
+         })
+         .catch(function(error){
+             console.log(error);
+         });
+
+     }
+    }
+  }
+};
+</script>
---
+++ reciclaje/src/components/Asociacion_Detail.vue
@@ -0,0 +1,174 @@
+<template>
+<v-layout align-start>
+    <v-flex>
+
+    <v-container >
+
+      <v-toolbar flat color="white">
+        <v-toolbar-title>Datos de la Asociacion</v-toolbar-title>
+         <v-spacer></v-spacer>
+ 
+      <v-btn icon>
+        <v-icon 
+        @click="validar"
+        >edit</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon
+         @click="validarDelete"
+         >delete</v-icon>
+      </v-btn>
+
+      </v-toolbar>
+      <v-divider ></v-divider>
+      <v-form v-model="valid">
+    <v-row class="first_row">
+        <v-col 
+        lg="2"
+        >
+        <p>ID</p>
+        </v-col>
+     
+        <v-col>
+        <v-text-field
+      v-model="asociacion.codigo"
+      :disabled="true"
+      label="Codigo"
+      required
+      solo
+    ></v-text-field>    
+        </v-col>
+    </v-row>
+    <v-row >
+        <v-col 
+        lg="2"
+        >
+        <p>Nombre</p>
+        </v-col>
+     
+        <v-col>
+        <v-text-field
+      v-model="asociacion.nombre"
+      :rules="nameRules"
+      :disabled="form_Disable"
+      label="Name"
+      required
+      solo
+    ></v-text-field>      
+        </v-col>
+    </v-row>
+      </v-form>
+</v-container>
+
+
+
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+    props: ['asociacionID'],
+    data() {
+    return {
+      asociacion: [],
+      dialog: false,
+       correct_data: true,
+
+      nameRules: [
+        v => !!v || 'Nombre es requirido',
+        v => (v && v.length <= 50) || 'El nombre debe ser menor a 50 caracteres',
+      ],
+      form_Disable: false,
+      //TODO:Model
+      codigo: '',
+      nombre:'',
+      valid: true,
+    };
+  },
+  computed: {
+    formTitle() {
+      return this.editedIndex === -1 ? "Nueva Asociacion" : "Actualizar Asociacion";
+    }
+  },
+
+  watch: {
+    dialog(val) {
+      val || this.close();
+    }
+  },
+
+  created() {
+    //TODO
+    this.listar();
+
+  },
+  methods: {
+    listar() {
+      //TODO
+      let me=this;
+      axios
+      .get("/asociacion/"+ this.asociacion)
+      .then(function(response){me.asociacion=response.data;
+      })
+      .catch(function(error){console.log(error);});
+
+    },
+    validar(){
+        const answer = window.confirm('Desea actualizar los datos ingresados?')
+
+        if(answer && this.valid){
+            this.guardar()
+        }else{
+
+        }
+    },
+    limpiar() {
+      this.codigo = "";
+      this.nombre = "";
+    },
+    validarDelete(){
+    const answer = window.confirm('Desea eliminar la asociación seleccionada?')
+
+        if(answer){
+            this.deleteItem()
+        }else{
+
+        }
+    },
+    deleteItem(){
+      let me = this;
+      axios.delete("/asociacion/"+me.asociacion.codigo)
+      .then(function(response){
+          window.location.href = '/asociacion';
+
+         })
+      .catch(function(error){
+              this.correct_data = false;
+             console.log(error);
+         });
+    },
+    guardar() {
+         let me =this;
+         axios
+         .put("/asociacion",{
+             codigo:me.asociacion.codigo,
+             nombre:me.asociacion.nombre
+         })
+         .then(function(response){
+            window.location.href = '/asociacion';
+
+         })
+         .catch(function(error){
+             console.log(error);
+         });
+     
+    }
+  }
+};
+</script>
+<style>
+  @import '../assets/details_view.css';
+</style>
