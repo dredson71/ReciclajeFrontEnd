d983627226897b1dc255dbef359affe2ff21266d
JALG99 <jorgelandaverry@hotmail.com>
2020-06-05 20:44:33 -0500

    Correción Dashboard Power BI

--- reciclaje/src/views/Home.vue
+++ reciclaje/src/views/Home.vue
@@ -1,5 +1,3 @@
 <template>
-   <div>
-     <iframe width="1300" height="800" src="https://app.powerbi.com/view?r=eyJrIjoiNzEyNjEyYTgtMGFhMi00YWFmLTliMGYtYmNhOGI3ZTdmNjkzIiwidCI6IjY2YzliMGUxLTA0MjgtNDE5Yy1hYTQwLWEzMzc4Mjg2ZDMzMiJ9" frameborder="0" allowFullScreen="true"></iframe>
-   </div>
+     <iframe width="100%" height="100%" src="https://app.powerbi.com/view?r=eyJrIjoiNzEyNjEyYTgtMGFhMi00YWFmLTliMGYtYmNhOGI3ZTdmNjkzIiwidCI6IjY2YzliMGUxLTA0MjgtNDE5Yy1hYTQwLWEzMzc4Mjg2ZDMzMiJ9" frameborder="0" allowFullScreen="true"></iframe>
 </template>
